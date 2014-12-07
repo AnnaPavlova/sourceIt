3967ff9ed5b07f48a9f7400b3d82634e2b659e73
SergeyLinnick <88.fcuk@gmail.com>
2014-12-07 23:44:38 +0200

    HomeTask8(2task_done)

---
+++ Linnick/HomeTask08/main.js
@@ -0,0 +1,56 @@
+var multiplicationTableHolder = document.getElementById('multiplication-table');
+var matrixHolder = document.getElementById('matrix');
+var pascalTriangleHolder = document.getElementById('pascal-triangle');
+
+matrixHolder.innerHTML =  render(matrix(10));
+multiplicationTableHolder.innerHTML = render(multiplicationTable(10));
+pascalTriangleHolder.innerHTML = render(pascal(10));
+	// двухмерный массив , вложеный цикл
+function multiplicationTable(size) {
+	var table = [];		// создаем массив
+	var i;				
+	var j;
+
+	for (i = 0; i < 10; i++){  // каждая итерация будет создавать массив с индексом i	
+		table[i] = [];	
+			
+			for (j = 0; j < 10; j++){	// внутрений цикл
+			table[i][j] = ((i + 1) * (j + 1)) //каждая итерация будет запонять двойной индекс ячейки
+		}
+	}
+	return table;
+}
+
+function matrix (size) {
+    var matrix = [];
+    // @todo
+    return matrix;
+}
+
+function pascal (size) {
+    var triangle = [];
+    var i;				
+	var j;
+	
+	for (i = 0; i < 9; i++){
+	triangle[i] = [];
+
+			for (j = 1; j <= i; j++){
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+			}
+		triangle[i][j] = 1;
+		triangle[i][0]= 1;
+	}
+    return triangle;
+}
+
+function render (array) {
+    var rowsQty = array.length;
+    var result = [];
+    for (var i = 0; i < rowsQty; i++) {
+        var row = ['<tr><td>', array[i].join('</td><td>'), '</td></tr>'].join('');
+        result.push(row);
+    }
+    return result.join('');
+}
+a/lert("Test");
