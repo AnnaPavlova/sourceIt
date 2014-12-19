1f48eea013e681da4d4286ec59af46e887a4a65c
Nataliia <gritaliana@gmail.com>
2014-12-19 11:24:42 +0200

    ht 12 calc/animation

--- Hridasova/ht_12/calc1/js/main.js
+++ Hridasova/ht_12/calc1/js/main.js
@@ -1,3 +1,7 @@
+function initPage() {
+	calculation();
+}
+
 function byClass(classList, node) {
  if(document.getElementsByClassName) {
   return (node || document).getElementsByClassName(classList);
@@ -21,21 +25,37 @@ function byClass(classList, node) {
  }
 }
 
-
-function initPage() {
+ function calculation() {
+	var inputRes = document.getElementById('result');
+	var container = document.getElementById('container');
+	var btnCalc = byClass('btn-calc', container)[0];
+	var calcResult, href, operator;
+	
+/*я пробовала остановиться на выборе имя узла <a>, записать в переменную  аттрибут href ->href = e.target.getAttribute('href'),
+и начав цикл перебора i < href.length делать сразу вычисления свичем, записывая рез-т в переменную.
+а потом результат записывать в текстовый инпут по нажатию ссылки btn-calc
+ т.е. вычисления происходили бы при нажатии на ссылку с оператором, а при нажатии на ссылку calculate опубликовывался бы результат, это возможно?*/
+	document.getElementById('operators').addEventListener('click', function(e) {
+		e.preventDefault();		
+		if(e.target && e.target.nodeName == 'a') {
+			operator = e.target.className.split(' ');//насколько я понимаю эта строка должна собирать все имена кдассов тегов <a> в массив,разделяя их пробелом..?
+			if(operator) {								
+				for(var i = 0; i < operator.length; i++) {
+					if(operator[i] == 'operator') {
+						operator[i].style.border = '1px solid green';
+						href = operator[i].getAttribute('href');
+					}
+				}
+			}
+		}
+	});
+	
+	btnCalc.addEventListener('click', function() {
 	var firstValue = parseInt(document.getElementById('first-value').value, 10);
 	var secondValue = parseInt(document.getElementById('second-value').value, 10);
-	var inputRes = document.getElementById('result').value;
-	var btnCalc = byClass('btn-calc');
-	var calcResult;
-	document.getElementById('operators').addEventListener('click', function(e) {
-		e.preventDefault();
-		if(e.target && e.target.nodeName == 'a') {
-			var href = e.target.getAttribute('href');
-			for (var i = 0; i < href.length; i++) {
-			switch(href[i]) {
+			switch(href) {
 				case '+':
-					calcResult = firstValue + secondValue;		
+					calcResult = firstValue + secondValue;
 					break;
 				case '-':
 					calcResult = firstValue - secondValue;
@@ -49,23 +69,8 @@ function initPage() {
 				default:
 					calcResult = 'no method chosen';
 			}
-			}
-			
-		}
 	});
-	
-	btnCalc.onclick = function (calcRes) {
-		calcRes = calcResult;
-		calcRes.preventDefault();
-		inputRes = calcRes;
-	}
-	
-	console.log('hello');
 }
-
-
-
-
 
 
 if (window.addEventListener)
