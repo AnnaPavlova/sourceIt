fac6404ec95f0dbf909a95a572b2d90aa3f1eda8
Anna Pavlova <ann.pavlova.mail@gmail.com>
2015-01-21 07:40:32 +0200

    Add hometask 19

---
+++ Hometasks/ht19/tasks/js/main.js
@@ -0,0 +1,15 @@
+function goto(level,button){
+    //TODO:return a number, how much levels the elevator must go up or down
+    return 0;
+}
+
+function sum() {
+    //TODO:return the sum of all arguments given.
+    return sum;
+}
+function double(array) {
+    //TODO: Return a new array with each value twice as large as the corresponding value in the passed in array.
+}
+function pluck(objs, name) {
+    //TODO: Returns an array containing the named property of each object
+}
